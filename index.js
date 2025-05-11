8569826f4ca8f5f2bd17fd60cad59f9b68bb988f
Atstincer <atstincer87@gmail.com>
2025-05-11 22:02:52 +0200

    3.14 phonebook database step 2

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-//const cors = require('cors')
+const Person = require('./models/person')
 
 const app = express()
 
 app.use(express.json())
-//app.use(morgan('tiny'))
 
 morgan.token('req-body',function(req,res){
     return JSON.stringify(req.body)
@@ -13,11 +13,9 @@ morgan.token('req-body',function(req,res){
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
 
-//app.use(cors())
-
 app.use(express.static('dist'))
 
-let persons = [
+/*let persons = [
     {
         "id": "1",
         "name": "Arto Hellas",
@@ -38,14 +36,42 @@ let persons = [
         "name": "Mary Poppendieck",
         "number": "39-23-6423122"
     }
-]
+]*/
 
 /*app.get('/',(request,response)=>{
     response.send('<h1>Hello World</h1>')
 })*/
 
+/*const user = 'atstincer87'
+const psw = process.argv[2]
+const dbName = 'phonebook'
+
+const url = `mongodb+srv://${user}:${psw}@cluster0.rni2sqj.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name:String,
+    number:String
+})
+
+personSchema.set('toJSON',{
+    transform:(document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+const Person = mongoose.model('Person',personSchema)*/
+
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    //response.json(persons)
+    Person.find({}).then(people => {
+        response.json(people)
+    })
 })
 
 app.get('/api/persons/:id',(request,response)=>{
@@ -82,8 +108,7 @@ const validate = (person) => {
 app.post('/api/persons',(request,response)=>{
     const person = request.body
     console.log(person)
-    const validation = validate(person)
-    //console.log(validation)
+    /*const validation = validate(person)
     if(!validation.value){
         return response.status(400).json({
             error: validation.msg
@@ -91,8 +116,17 @@ app.post('/api/persons',(request,response)=>{
     }
     person.id = String(getNextId())
     //console.log(person)
-    persons = persons.concat(person)
-    response.json(person)
+    persons = persons.concat(person)*/
+    if(person){
+        const p = new Person({
+            name:person.name,
+            number:person.number
+        })
+        p.save().then(personSaved => {
+            response.json(personSaved)
+        })
+    }
+    //response.json(person)
 })
 
 app.delete('/api/persons/:id',(request,response)=>{
